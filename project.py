bd74903ad3a485b06f55d9d5c04317884e363242
Sam Elliott <soe1@uakron.edu>
2025-09-27 15:43:49 -0400

    updated the python script to be dockerized easier

--- project.py
+++ project.py
@@ -1,4 +1,9 @@
+# fetch_pokemon.py
 import requests
 
-response = requests.get("https://pokeapi.co/api/v2/pokemon/absol")
-print(response.json()["name"])
+def main():
+    response = requests.get("https://pokeapi.co/api/v2/pokemon/absol")
+    print(response.json()["name"])
+
+if __name__ == "__main__":
+    main()
